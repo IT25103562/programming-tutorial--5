7b35cc1f19e7a0321d34ff02b211445b723214e4
IT25103562 <IT25103562@my.sliit.lk>
2026-02-08 21:07:37 +0530

    Add files via upload

---
+++ IT25103562_lab.java
@@ -0,0 +1,29 @@
+Calculator Class:
+
+class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int square(int a) {
+        return a * a;
+    }
+}
+
+Main Class:
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+
+        int result1 = cal.square(cal.add(cal.multiply(3,4), cal.multiply(5,7)));
+        int result2 = cal.square(cal.add(4,7)) + cal.square(cal.add(8,3));
+
+        System.out.println("Result 1: " + result1);
+        System.out.println("Result 2: " + result2);
+    }
+}
